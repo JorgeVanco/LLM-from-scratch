6dac43d4a3ab21ad6b69ff71ea2698c5bd60524e
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2025-06-21 21:14:47 +0200

    chore: bpe using finditer

--- src/tokenizer/bpe.py
+++ src/tokenizer/bpe.py
@@ -45,17 +45,23 @@ class BPE:
         return byte_text
 
     def train(self, input_path: str, vocab_size: int, special_tokens: list[str]) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
+        
+        assert vocab_size > 256, "Vocabulary size must be greater than 256"
+        
         file = open(input_path, "r", encoding="utf-8")
         # chunk_boundaries = find_chunk_boundaries(file, desired_num_chunks, split_special_tokens)
         text = file.read()
         file.close()
         
-        pretokenized_text = re.findall(self.PATTERN, text)
+        pretokenized_text = re.finditer(self.PATTERN, text)
+        byte_text: list[list[bytes]] = [tuple(c.encode("utf-8") for c in pretoken.group()) for pretoken in pretokenized_text]
+
         vocab: dict[int,bytes] = {i: bytes([i]) for i in range(256)}
-        byte_text: list[list[bytes]] = [tuple([bytes(chr(c), "utf-8") for c in bytes(pretoken, "utf-8")]) for pretoken in pretokenized_text]
         current_vocab_size: int = len(vocab)
-        num_merges = vocab_size - current_vocab_size - len(special_tokens)
         merges: list[tuple[bytes, bytes]] = list()
+        
+        num_merges = vocab_size - current_vocab_size - len(special_tokens)
+        
         for _ in range(num_merges):
             
             max_pair = self._get_max_pair(byte_text)
