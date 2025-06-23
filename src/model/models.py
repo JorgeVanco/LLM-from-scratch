41c33c9c18167005bd65f3d3038793e8730da9fb
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2025-06-23 19:44:23 +0200

    feat(models): embedding layer

--- src/model/models.py
+++ src/model/models.py
@@ -13,3 +13,14 @@ class Linear(nn.Module):
     
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return einsum(x, self.weight, "... in_features, out_features in_features -> ... out_features")
+    
+    
+class Embedding(nn.Module):
+    def __init__(self, num_embeddings: int, embedding_dim: int, device: torch.device | None = None, dtype: torch.dtype | None = None) -> None:
+        super().__init__()
+        self.weight = nn.Parameter(torch.empty(num_embeddings, embedding_dim, device=device, dtype=dtype))
+        
+        torch.nn.init.trunc_normal_(self.weight, 0.0, 1.0, a = -3, b = 3)
+        
+    def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
+        return self.weight[token_ids]
