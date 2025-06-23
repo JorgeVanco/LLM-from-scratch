dea42e739972c4a644d9cd1bb895d4fda61639a2
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2025-06-23 23:08:01 +0200

    feat(models): RMSNorm

--- src/model/models.py
+++ src/model/models.py
@@ -24,3 +24,22 @@ class Embedding(nn.Module):
         
     def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
         return self.weight[token_ids]
+    
+
+class RMSNorm(nn.Module):
+    def __init__(self, d_model: int, eps: float = 1e-5, device: torch.device | None = None, dtype: torch.dtype | None = None) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(d_model, device=device, dtype=dtype))
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        in_dtype = x.dtype
+        x = x.to(torch.float32)
+        
+        RMS = (self.eps + x.square().sum(dim=-1, keepdim=True) / self.d_model).sqrt()
+        result = x / RMS * self.weight
+        
+        result = result.to(in_dtype)
+        
+        return result
