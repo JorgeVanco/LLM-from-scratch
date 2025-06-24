f002649036673ee376dcfcc6305b86566bb4b5a1
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2025-06-24 22:00:13 +0200

    feat(utils): cross_entropy

---
+++ src/utils/loss.py
@@ -0,0 +1,14 @@
+import torch
+from torch import Tensor
+from jaxtyping import Float, Int
+from einops import reduce
+
+def cross_entropy(logits: Float[Tensor, "batch_size vocab_size"], targets: Int[Tensor, "batch_size"]) -> Tensor:
+    
+    batch_size, vocab_size = logits.shape
+    
+    max_logit = logits.amax(dim=-1, keepdim=True)
+    
+    logits_subtracted = logits - max_logit
+    return (- logits_subtracted[torch.arange(batch_size), targets] + logits_subtracted.exp().sum(-1).log()).mean()
+    return reduce(- logits_subtracted[torch.arange(batch_size), targets] + logits_subtracted.exp().sum(-1).log(), "... batch_size -> ", "mean")
