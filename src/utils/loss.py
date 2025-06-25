c03fcfec00661b843024f3c70eaf1570009b1e3a
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2025-06-25 09:10:22 +0200

    feat(loss)": cross_entropy loss

--- src/utils/loss.py
+++ src/utils/loss.py
@@ -5,7 +5,7 @@ from einops import reduce
 
 def cross_entropy(logits: Float[Tensor, "batch_size vocab_size"], targets: Int[Tensor, "batch_size"]) -> Tensor:
     
-    batch_size, vocab_size = logits.shape
+    batch_size = logits.size(0)
     
     max_logit = logits.amax(dim=-1, keepdim=True)
     
