6971cfc61100c16b59bceec6ef1ac04070f290d0
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2025-07-16 17:28:44 +0200

    perf(ddp): flattened gradient communication

--- src/distributed/ddp.py
+++ src/distributed/ddp.py
@@ -11,11 +11,16 @@ class DDPIndividualParameters(torch.nn.Module):
             dist.broadcast(param.data, src=0)
             
     def finish_gradient_synchronization(self) -> None:
+        params = [param.grad.data / dist.get_world_size() for param in self.module.parameters() if param.requires_grad]
+        params_flatten = torch._utils._flatten_dense_tensors(params)
+        dist.all_reduce(params_flatten, op=dist.ReduceOp.SUM)
+        params_unflatten = torch._utils._unflatten_dense_tensors(params_flatten, params)
+        
+        i = 0
         for param in self.module.parameters():
             if param.requires_grad:
-                param.grad.data /= dist.get_world_size()
-                dist.all_reduce(param.grad.data, op=dist.ReduceOp.SUM)
-
+                param.grad.data = params_unflatten[i]
+                i += 1
 
     def forward(self, *args, **kwargs) -> torch.Tensor:
         return self.module(*args, **kwargs)
