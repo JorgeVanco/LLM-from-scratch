bc9e580e6d5c00a5e8018266a3c051b2d8a4ebc0
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2025-07-16 17:51:24 +0200

    perf(ddp): overlapping computation and communication with register_post_accumulate_grad_hook

--- src/distributed/ddp.py
+++ src/distributed/ddp.py
@@ -6,21 +6,23 @@ class DDPIndividualParameters(torch.nn.Module):
         super().__init__()
         self.module = module
         self.device = next(module.parameters()).device
+        self.handles = []
+        world_size = dist.get_world_size()
+        
+        def hook(param: torch.Tensor) -> None:
+            param.grad.data /= world_size
+            handle = dist.all_reduce(param.grad, op=dist.ReduceOp.SUM, async_op = True)
+            self.handles.append(handle)
         
         for param in module.parameters():
             dist.broadcast(param.data, src=0)
+            if param.requires_grad:
+                param.register_post_accumulate_grad_hook(hook)
             
     def finish_gradient_synchronization(self) -> None:
-        params = [param.grad.data / dist.get_world_size() for param in self.module.parameters() if param.requires_grad]
-        params_flatten = torch._utils._flatten_dense_tensors(params)
-        dist.all_reduce(params_flatten, op=dist.ReduceOp.SUM)
-        params_unflatten = torch._utils._unflatten_dense_tensors(params_flatten, params)
-        
-        i = 0
-        for param in self.module.parameters():
-            if param.requires_grad:
-                param.grad.data = params_unflatten[i]
-                i += 1
+        for handle in self.handles:
+            handle.wait()
+        self.handles.clear()
 
     def forward(self, *args, **kwargs) -> torch.Tensor:
         return self.module(*args, **kwargs)
