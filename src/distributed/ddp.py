aa5c516a69d9e8a91762874de5b21de29637e8f6
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2025-07-16 23:06:51 +0200

    feat(ddp): ShardedOptimizer

--- src/distributed/ddp.py
+++ src/distributed/ddp.py
@@ -1,5 +1,8 @@
 import torch
 import torch.distributed as dist
+import numpy as np
+from collections import defaultdict
+from typing import Type, Any, Iterable, Optional, Callable
 
 class DDPIndividualParameters(torch.nn.Module):
     def __init__(self, module: torch.nn.Module) -> None:
@@ -84,3 +87,48 @@ class DDPBucket:
             handle = dist.all_reduce(flat_gradients, op=dist.ReduceOp.SUM, async_op=True)
             return handle, flat_gradients
         return None, None
+    
+
+class ShardedOptimizer(torch.optim.Optimizer):
+    def __init__(self, params: Iterable[torch.nn.Parameter | dict], optimizer_cls: Type[torch.optim.Optimizer], **kwargs: Any) -> None:
+        params = list(params)
+        self.optimizer = None
+        self.optimizer_cls = optimizer_cls
+        self.kwargs = kwargs
+        self.rank: int = dist.get_rank()
+        self.world_size: int = dist.get_world_size()
+        self._rank_params: defaultdict = defaultdict(list)
+        self._size_per_rank: list = [0] * self.world_size
+        super().__init__(params, {})
+    
+    def step(self, closure: Optional[Callable] = None, **kwargs) -> None:
+        self.optimizer.step(closure=closure, **kwargs)
+        self._sync()
+        
+    def _sync(self) -> None:
+        handles = []
+        for rank in self._rank_params:
+            for p in self._rank_params[rank]:
+                handle = dist.broadcast(p.data, src=rank, async_op = True)
+                handles.append(handle)
+                
+        _ = [h.wait() for h in handles]
+        
+    def add_param_group(self, param_group: dict[str, Any]) -> None:
+        super().add_param_group(param_group)
+
+        param_group_to_add = defaultdict(list)
+        for group_name, params in param_group.items():
+            for p in params:
+                min_rank = np.argmin(self._size_per_rank)
+                self._rank_params[min_rank].append(p)
+                self._size_per_rank[min_rank] += p.data.nbytes
+                
+                if self.rank == min_rank:
+                    param_group_to_add[group_name].append(p)
+
+        if self.optimizer is None:
+            self.optimizer = self.optimizer_cls([param_group_to_add], **self.kwargs)
+            del self.kwargs
+        else:
+            self.optimizer.add_param_group(param_group_to_add)
