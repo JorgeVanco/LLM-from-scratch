ce7bc76713d71246856ddca86bd26b47cc4837a3
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2025-07-17 10:46:56 +0200

    fix(utils): casting to float32 in softmax

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,9 +2,14 @@ import torch
 
 
 def softmax(x: torch.Tensor, dim: int) -> torch.Tensor:
+    in_dtype = x.dtype
+    x = x.to(torch.float32)
+    
     max_x = x.amax(dim=dim, keepdim=True)
     exponentiated_x = (x - max_x).exp()
 
     sftmax = exponentiated_x / exponentiated_x.sum(dim=dim, keepdim=True)
+    
+    sftmax = sftmax.to(in_dtype)
 
     return sftmax
