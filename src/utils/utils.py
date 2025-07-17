fix(softmax): using torch.amp to cast to float32 because manually casting does not work unless having a print statement in the middle of the function

src/utils/utils.py
@@ -1,15 +1,12 @@
 import torch
 
-
+# Note: For some reason casting the input to float32 does not work unless you put a print statement anywhere in the function
+torch.amp.custom_fwd(device_type='cuda', cast_inputs=torch.float32)
 def softmax(x: torch.Tensor, dim: int) -> torch.Tensor:
-    in_dtype = x.dtype
-    x = x.to(torch.float32)
-    
+
     max_x = x.amax(dim=dim, keepdim=True)
     exponentiated_x = (x - max_x).exp()
 
     sftmax = exponentiated_x / exponentiated_x.sum(dim=dim, keepdim=True)
-    
-    sftmax = sftmax.to(in_dtype)
 
     return sftmax